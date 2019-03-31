587ce2f5b97c96c8caea3bd45c45275fa5eeb905
Nicolas Caif <r.nicolascaif@gmail.com>
2019-03-31 20:39:23 -0300

    updated links and profile

---
+++ javascript/baffle.js
@@ -0,0 +1,15 @@
+const text = baffle('.data');
+text.set({
+  characters : 'zpxzVpasdfh86136░█▒ ░░░█▓ >░░ ▓/▒█▓ █ █>█▒sayg▒ ░░░█▓ >yf',
+  speed: 90
+});
+text.start();
+text.reveal(70000);
+
+const text1 = baffle('.quote');
+text1.set({
+  characters : 'qwertyuiopasdfgh8613611888',
+  speed: 150
+});
+text1.start();
+text1.reveal(5500);
